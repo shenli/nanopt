b9aa50c8bae3677e89d15c3c15f8b7da34c35706
Li Shen <shenli3514@gmail.com>
2026-08-04 20:12:15 -0700

    docs: publish clean site snapshot

---
+++ javascripts/mathjax.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$", "$"]],
+    displayMath: [["$$", "$$"]],
+    processEscapes: true,
+  },
+  options: {
+    ignoreHtmlClass: "tex2jax_ignore",
+    processHtmlClass: "tex2jax_process",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
